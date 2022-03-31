32430f066cf093361a042f047b8454d244fcb028
Andrew Fitzgerald <fitzgeralda@sureskills.com>
2022-03-31 18:59:05 +0100

    chore: add user id to the movies create form

--- Data/Movie.cs
+++ Data/Movie.cs
@@ -13,6 +13,8 @@ namespace MoviesUI.Data
         [DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
 
+        public Guid CreatedBy { get; set; }
+
         public string Genre { get; set; }   
 
         public decimal Price { get; set; }  
